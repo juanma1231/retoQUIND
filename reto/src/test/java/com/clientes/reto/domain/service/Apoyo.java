f6104ad147a4438013aaf0a9a6c883d09c5a0149
user <juan.zuluaga4461@uco.net.co>
2023-07-05 11:03:57 -0500

    Pruebas unitarias PersonService

---
+++ reto/src/test/java/com/clientes/reto/domain/service/Apoyo.java
@@ -0,0 +1,72 @@
+package com.clientes.reto.domain.service;
+
+import static org.mockito.BDDMockito.given;
+
+import com.clientes.reto.domain.dto.PersonDto;
+import com.clientes.reto.domain.repository.IPersonRepository;
+import com.clientes.reto.domain.usecase.IPersonUseCase;
+import com.clientes.reto.utils.CustomException;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest
+class Apoyo {
+    @MockBean
+    private IPersonRepository iPersonRepository;
+
+    @Autowired
+    private PersonService personService;
+    @BeforeEach
+    void setUp(){
+        iPersonRepository = mock(IPersonRepository.class);
+        personService = new PersonService();
+    }
+
+    @Test
+    void getAllUsers() {
+        System.out.println("hola");
+    }
+
+    @Test
+    void create() {
+        PersonDto personDto = new PersonDto();
+        personDto.setName("Juan manuel");
+        personDto.setAge(19);
+        personDto.setEmail("Juan Mnauel");
+        Date fechaNacimiento = new Date(99, 4, 8);
+        personDto.setBirthDay(fechaNacimiento);
+        personDto.setCreationDate(new Date());
+        personDto.setIdType("CEDULA");
+        personDto.setUpdateDate(new Date());
+        personDto.setIdNumber(1234567890);
+        when(iPersonRepository.save(personDto)).thenReturn(personDto);
+        when(iPersonRepository.finById(personDto.getEmail())).thenReturn(null);
+        given(iPersonRepository.finById(personDto.getEmail())).willReturn(null);
+        given(iPersonRepository.save(personDto)).willReturn(personDto);
+
+        PersonDto personaGuardada = personService.create(personDto);
+
+        assertThat(personaGuardada).isNotNull();
+
+
+    }
+}
--- reto/src/test/java/com/clientes/reto/domain/service/PersonServiceTest.java
+++ reto/src/test/java/com/clientes/reto/domain/service/PersonServiceTest.java
@@ -1,42 +1,98 @@
 package com.clientes.reto.domain.service;
-
+import static org.mockito.BDDMockito.given;
 import com.clientes.reto.domain.dto.PersonDto;
 import com.clientes.reto.domain.repository.IPersonRepository;
-import com.clientes.reto.domain.usecase.IPersonUseCase;
 import com.clientes.reto.utils.CustomException;
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
-    @Autowired
-    IPersonUseCase iPersonUseCase;
+    @Mock
+    private IPersonRepository iPersonRepository;
+
+    @InjectMocks
+    private PersonService personService;
+    PersonDto personDto = new PersonDto();
+    @BeforeEach
+    void setUp(){
+
+        personDto.setName("Juan manuel");
+        personDto.setAge(19);
+        personDto.setEmail("Juan Mnauel");
+        Date fechaNacimiento = new Date(99, 4, 8);
+        personDto.setBirthDay(fechaNacimiento);
+        personDto.setCreationDate(new Date());
+        personDto.setIdType("CEDULA");
+        personDto.setUpdateDate(new Date());
+        personDto.setIdNumber(1234567890);
+
+    }
+
     @Test
     void getAllUsers() {
+        PersonDto personDto1 = new PersonDto();
+        personDto1.setName("Juan manuel");
+        personDto1.setAge(19);
+        personDto1.setEmail("Juan@gmail.com");
+        Date fechaNacimiento = new Date(99, 4, 8);
+        personDto1.setBirthDay(fechaNacimiento);
+        personDto1.setCreationDate(new Date());
+        personDto1.setIdType("CEDULA");
+        personDto1.setUpdateDate(new Date());
+        personDto1.setIdNumber(1234567890);
+        given(iPersonRepository.getAllUsers()).willReturn(List.of(personDto,personDto1));
+        List<PersonDto> list = (List<PersonDto>) personService.getAllUsers();
+        assertThat(list.size()).isEqualTo(2);
+
+
     }
 
     @Test
     void create() {
+        //given
+        given(iPersonRepository.finById(personDto.getEmail())).willReturn(null);
+        given(iPersonRepository.save(personDto)).willReturn(personDto);
+        //when
+        PersonDto personaGuardada = personService.create(personDto);
+        //then
+        assertThat(personaGuardada).isNotNull();
+    }
+    @Test
+    void creste_with_exeption(){
+        given(iPersonRepository.finById(personDto.getEmail())).willReturn(personDto);
+        assertThrows(CustomException.class,()->{
+            personService.create(personDto);
+        });
 
-
-        PersonDto personDto = new PersonDto();
-        personDto.setAge(19);
-        personDto.setEmail("juan@gmail.com");
-        personDto.setName("juan manuel");
-        Mockito.when(iPersonUseCase.findById(Mockito.anyString())).thenReturn(null);
-        System.out.println(iPersonUseCase.create(personDto));
-        Assertions.assertEquals(PersonDto.class,iPersonUseCase.create(personDto));
+        verify(iPersonRepository,never()).save(personDto);
     }
 
     @Test
     void delete() {
+
     }
 
     @Test
     void findById() {
+        given(iPersonRepository.finById(personDto.getEmail())).willReturn(personDto);
+        PersonDto personDto1 = personService.findById(personDto.getEmail());
+        assertThat(personDto1).isNotNull();
+
     }
 
     @Test
